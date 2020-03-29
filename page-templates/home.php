7546182fd2621cfa99e274eaaee2b90b5de9d2f2
ceudasantaluz <56213679+ceudasantaluz@users.noreply.github.com>
2020-03-28 23:41:39 -0400

    home page and start editing the header

---
+++ page-templates/home.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'loop-templates/content', 'empty' );
+endwhile;
+
+?>
+
+<!-- BEGIN CONTENT -->
+
+
+
+<!-- END CONTENT -->
+
+<?php get_footer(); ?>
