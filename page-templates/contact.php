a9408a549620746a0ee270b8016f800c00a990b5
ceudasantaluz <56213679+ceudasantaluz@users.noreply.github.com>
2020-03-29 11:21:41 -0400

    update to theme.scss

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home
+ * Template Name: Contact
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
@@ -22,10 +22,11 @@ endwhile;
 
 <!-- BEGIN CONTENT -->
 
-<div class="row">
-	<div class="col-12">
-		<p>test sample</p>
+<div class="swl-flex-row">
+	<div class="left-col-image">
+
 	</div>
+	
 </div>
 
 <!-- END CONTENT -->
