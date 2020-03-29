13f5309a4779387aee62b118c608448b03abade4
ceudasantaluz <56213679+ceudasantaluz@users.noreply.github.com>
2020-03-29 11:31:49 -0400

    Update contact.php

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -28,10 +28,22 @@ endwhile;
 	</div>
 	<div class="right-content conact-vertical-split">
 		<div class="contact-summary">
-
+			<div class="row">
+				<div class="col">
+					<p>410 Adelaide Street West
+					<br>Suite 220
+					<br>Toronto, Ontario
+					<br>M5V 1S8</p>
+				</div>
+				<div class="col">
+					<p>T: 647-361-4410</p>
+					<p>F: 647-361-3143</p>
+					<p>info@seabrookworkplacelaw.ca</p>
+				</div>
+			</div>
 		</div>
 		<div class="contact-form">
-			
+			<p>[contact form]</p>
 		</div>
 	</div>
 </div>
