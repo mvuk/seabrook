cf7ac3cd0441b58a3ef9ecffc46ae559920f90dc
ceudasantaluz <56213679+ceudasantaluz@users.noreply.github.com>
2020-04-13 11:53:39 -0400

    legal services page update

--- page-templates/legal-services.php
+++ page-templates/legal-services.php
@@ -37,51 +37,53 @@ endwhile;
 			<h2>For Employees</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.  Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			<div class="service-links">
-				<div class="service-link-col">
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-				</div>
-				<div class="service-link-col">
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
+				<div class="row">
+					<div class="col">
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+					</div>
+					<div class="col">
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="vertical-stack-service-group">
 		<div class="service-group">
 			<h2>For Employers</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.  Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			<div class="service-links">
-				<div class="service-link-col">
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-				</div>
-				<div class="service-link-col">
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
-					<a href="#">Sample Service</a>
+				<div class="row">
+					<div class="col">
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+					</div>
+					<div class="col">
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+						<a href="#">Sample Service</a>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="vertical-stack-service-group">
 		<div class="service-group">
 			<h2>Workplace Investigations and Training</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.  Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			<div class="service-links">
-				<div class="service-link-col">
-					<a href="#">Workplace Investigations</a>
-					<a href="#">Workplace Climate Reviews</a>
-					<a href="#">Workplace Restorative Mediation</a>
-				</div>
-				<div class="service-link-col">
-					<a href="#">Workplace Training</a>
+				<div class="row">
+					<div class="col">
+						<a href="#">Workplace Investigations</a>
+						<a href="#">Workplace Climate Reviews</a>
+						<a href="#">Workplace Restorative Mediation</a>
+					</div>
+					<div class="col">
+						<a href="#">Workplace Training</a>
+					</div>
 				</div>
 			</div>
 		</div>
