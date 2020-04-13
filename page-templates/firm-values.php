dd19743768e4fa55aa105159ff68654feea5c3b3
ceudasantaluz <56213679+ceudasantaluz@users.noreply.github.com>
2020-04-13 15:57:26 -0400

    make templates more universal

--- page-templates/firm-values.php
+++ page-templates/firm-values.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Firm Values Services
+ * Template Name: Firm Values
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
