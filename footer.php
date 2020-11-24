25b92ce71f52765b5de10df2ccb5ecf7c1ef3be7
Matthew Vuk <matthewvuk@gmail.com>
2020-11-23 21:52:17 -0500

    footer outline

--- footer.php
+++ footer.php
@@ -16,22 +16,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
 <div class="swl-footer">
-	<div class="row">
-		<div class="col-md-6">
-			<!--  -->
-			<p>footer menu</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<!--  -->
+				<p>footer menu</p>
 
-			<!--  -->
+				<!--  -->
+			</div>
+			<div class="col-md-6">
+				<p>Follow us on Social Media</p>
+				<p>Instagram images</p>
+				<p>Footer social links</p>
+			</div>
 		</div>
-		<div class="col-md-6">
-			<p>Follow us on Social Media</p>
-			<p>Instagram images</p>
-			<p>Footer social links</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<p>Seabrook Workplace Law 2020. All Rights Reserved.</p>
+		<div class="row">
+			<div class="col-md-12">
+				<p>Seabrook Workplace Law 2020. All Rights Reserved.</p>
+			</div>
 		</div>
 	</div>
 </div>
