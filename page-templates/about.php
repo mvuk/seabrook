050e4ccf8f4c0be005b375ffabb73a71f9f0b09b
Matthew Vuk <matthewvuk@gmail.com>
2020-11-23 22:05:23 -0500

    add page templates

---
+++ page-templates/about.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'loop-templates/content', 'empty' );
+endwhile;
+
+?>
+
+<div class="swl-dark-banner">
+	<div class="swl-dark-banner-content">
+
+		<p>Quote about SWL placed here</p>
+
+	</div>
+</div>
+
+<p>about</p>
+
+<?php
+
+get_footer();
