df374d7bf5e4dc7bb14d8f9a6a37d7d09be248ba
Matthew Vuk <matthewvuk@gmail.com>
2021-01-27 23:58:18 -0500

    update

--- footer.php
+++ footer.php
@@ -17,6 +17,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <p>footer</p>
 
+	<!-- FOOTER RESOURCES -->
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
 <?php wp_footer(); ?>
 
 </body>
