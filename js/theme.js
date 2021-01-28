85925133e38b2bf2f5a7e3822cda5e011c2b3cc2
Matthew Vuk <matthewvuk@gmail.com>
2021-01-28 17:07:13 -0500

    update

--- js/theme.js
+++ js/theme.js
@@ -7057,8 +7057,10 @@
 })();
 function openMenu() {
   console.log('openMenu');
+  document.getElementById('overlayMenu').removeClass('displayNone');
 }
 
 function closeMenu() {
   console.log('closeMenu');
+  document.getElementById('overlayMenu').addClass('displayNone');
 }
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,7 +1,9 @@
 function openMenu() {
   console.log('openMenu');
+  document.getElementById('overlayMenu').removeClass('displayNone');
 }
 
 function closeMenu() {
   console.log('closeMenu');
+  document.getElementById('overlayMenu').addClass('displayNone');
 }
