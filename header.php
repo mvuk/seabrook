b6bd2ef9c8a28b41559c272d817236085232ed05
Matthew Vuk <matthewvuk@gmail.com>
2021-01-28 00:00:33 -0500

    update

--- header.php
+++ header.php
@@ -18,6 +18,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<link rel="stylesheet" href="https://use.typekit.net/udh7lti.css">
+
+
 	<?php wp_head(); ?>
 </head>
 
