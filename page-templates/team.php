67e98c4a8476af3dc024c520c7e256994c7acdb2
Matthew Vuk <matthewvuk@gmail.com>
2021-01-28 23:32:08 -0500

    team page

--- page-templates/team.php
+++ page-templates/team.php
@@ -20,53 +20,57 @@ endwhile;
 
 ?>
 
-<div class="half-container">
+<div class="page-halves">
 
-	<div class="left-area">
-		<div class="left-area-container">
-			<img src="/wp-content/uploads/2021/01/SWL_Logo_Web_White.png" alt="" class="logo">
-			<h1>Who makes it happen?</h1>
-			<p>Our legal team is top notch lawyers only. They are competent and happy and skilled.</p>
-			<p>Explore the profiles of our lawyers.</p>
+	<div class="half-container">
+
+		<div class="left-area">
+			<div class="left-area-container">
+				<img src="/wp-content/uploads/2021/01/SWL_Logo_Web_White.png" alt="" class="logo">
+				<h1>Who makes it happen?</h1>
+				<p>Our legal team is top notch lawyers only. They are competent and happy and skilled.</p>
+				<p>Explore the profiles of our lawyers.</p>
+			</div>
 		</div>
+
 	</div>
 
-</div>
+	<div class="page-content half-page">
 
-<div class="page-content half-page">
+		<div class="row">
+			<div class="col-md-12">
 
-	<div class="row">
-		<div class="col-md-12">
-
-			<div class="team-card">
-				<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-6189.jpg" alt="">
-				<div class="content">
-					<h2>Samantha<br>Seabrook</h2>
-					<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
-					<a href="/our-legal-team/samantha-seabrook/">Get to know her</a>
+				<div class="team-card">
+					<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-6189.jpg" alt="">
+					<div class="content">
+						<h2>Samantha<br>Seabrook</h2>
+						<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
+						<a href="/our-legal-team/samantha-seabrook/">Get to know her</a>
+					</div>
 				</div>
-			</div>
 
-			<div class="team-card">
-				<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-6037.jpg" alt="">
-				<div class="content">
-					<h2>Karina<br>Pogosyan</h2>
-					<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
-					<a href="/our-legal-team/karina-pogosyan/">Get to know her</a>
+				<div class="team-card">
+					<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-6037.jpg" alt="">
+					<div class="content">
+						<h2>Karina<br>Pogosyan</h2>
+						<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
+						<a href="/our-legal-team/karina-pogosyan/">Get to know her</a>
+					</div>
 				</div>
-			</div>
 
-			<div class="team-card">
-				<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-5973.jpg" alt="">
-				<div class="content">
-					<h2>Sanam<br>Nazir</h2>
-					<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
-					<a href="/our-legal-team/sanam-nazir/">Get to know her</a>
+				<div class="team-card">
+					<img src="/wp-content/uploads/2021/01/201108-RV-Seabrook-WPL-5973.jpg" alt="">
+					<div class="content">
+						<h2>Sanam<br>Nazir</h2>
+						<p>Our founder and visionary woman. Samantha is warm, sharp, and ready to give you confidence in your case.</p>
+						<a href="/our-legal-team/sanam-nazir/">Get to know her</a>
+					</div>
 				</div>
+
+
 			</div>
-
-
 		</div>
+
 	</div>
 
 </div>
