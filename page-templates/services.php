5bf9a991be6401b33976b6fb6b6774526c91d269
Matthew Vuk <matthewvuk@gmail.com>
2021-02-10 15:24:01 -0500

    update

--- page-templates/services.php
+++ page-templates/services.php
@@ -47,21 +47,21 @@ endwhile;
 				<div class="service-group">
 					<h2>Investigations & Training</h2>
 					<p>Short summary about this service. Short summary about this service. Short summary about this service.</p>
-					<a href="/workplace-law-services/investigations-training/">Learn more</a>
+					<a href="/workplace-law-services/investigations-training/" class="pill-button pink">Learn more</a>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="service-group">
 					<h2>Employees</h2>
 					<p>Short summary about this service. Short summary about this service. Short summary about this service.</p>
-					<a href="/workplace-law-services/employees">Learn more</a>
+					<a href="/workplace-law-services/employees" class="pill-button pink">Learn more</a>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="service-group">
 					<h2>Employers</h2>
 					<p>Short summary about this service. Short summary about this service. Short summary about this service.</p>
-					<a href="/workplace-law-services/employers">Learn more</a>
+					<a href="/workplace-law-services/employers" class="pill-button pink">Learn more</a>
 				</div>
 			</div>
 
