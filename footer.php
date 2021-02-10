cfba71cdd11fa3f5fdc526738a191d3274c073aa
Matthew Vuk <matthewvuk@gmail.com>
2021-02-09 20:31:37 -0500

    footer

--- footer.php
+++ footer.php
@@ -15,33 +15,37 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="footer">
+<div class="footer one">
 
   <div class="container">
     <div class="row footer-row-one">
-      <div class="col-md-4">
-
-      </div>
-      <div class="col-md-4">
-
-      </div>
-      <div class="col-md-4">
-
-      </div>
-      <div class="col-md-4">
-
-      </div>
-    </div>
-    <div class="row footer-row-two">
       <div class="col-md-12">
-        <p>© 2021 Seabrook Workplace Law</p>
+        <?php echo do_shortcode('[contact-form-7 id="105"]'); ?>
       </div>
-
     </div>
+
   </div>
 
 </div>
 
+<div class="footer two">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-4">
+
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php wp_footer(); ?>
 
